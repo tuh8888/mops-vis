1af071513443a1ae69f0820b98e01fbc857eb261
tuh8888 <pielkekid@gmail.com>
2018-10-30 13:36:52 -0600

    Reordered functions and fixed path processing

--- src/common-lisp/mops-vis.lisp
+++ src/common-lisp/mops-vis.lisp
@@ -2,87 +2,100 @@
 
 (in-package :KaBOB)
 
-;;;;;;;;; JSON ;;;;;;;;;;
-
-(defun inherited-role? (mop role)
-  (if (find role (mop-roles mop)) t nil))
-
-(defun make-role-links (mop role)
-  (let ((fillers (inherit-filler mop role)))
-    (cond ((listp fillers) (mapcar #'(lambda (filler) (net-vis:make-link :source mop
-                                                                 :label role
-                                                                 :target filler
-                                                                 :data (inherited-role? mop role)))
-                                   (inherit-filler mop role)))
-          (t (list (net-vis:make-link :source mop
-                              :label role
-                              :target fillers
-                              :data (inherited-role? mop role)))))))
-
-(defun make-abstraction-links (mop)
-  (mapcar #'(lambda (abstraction) (net-vis:make-link :source mop
-                                             :label "subClassOf"
-                                             :target abstraction
-                                             :data nil))
-          (mop-abstractions mop)))
-
-(defun make-mop-links (mop data)
-  `(,@(make-abstraction-links mop)
-    ,@(mapcan #'(lambda (role) (make-role-links mop role)) (vis-roles mop data))))
+;;; UTIL ;;;
 
 (defun vis-roles (mop data)
   (filter-roles (if (first data)                    
                     (inheritable-roles mop)
                     (mop-roles mop))))
 
-(defun make-filler-nodes (mop role)
+(defun filter-roles (roles)
+  (remove-if #'(lambda (role) (equal :kabob-ids role)) roles))
+
+(defun inherited-role? (mop role)
+  (if (find role (mop-roles mop)) t nil))
+
+(defun find-node (node-name)
+  (lookup-mop (intern node-name :KaBOB)))
+
+(defun find-relationship (source target)
+  (let ((relationship nil))
+    (dolist (role (mop-roles source) 'done)
+      (dolist (filler (role-filler source role) 'done)
+        (when (equal filler target)
+          (setf relationship role))))
+    (unless relationship
+      (dolist (abstraction (mop-abstractions source) 'done)
+        (when (equal abstraction target)
+          (setf relationship "subClassOf"))))
+    relationship))
+
+
+;;; LINKS ;;;
+
+(defun make-mops-links (mops data)
+  (mapcan #'(lambda (mop) (make-mop-links mop data)) mops))
+
+(defun make-mop-links (mop data)
+  `(,@(make-abstraction-links mop)
+    ,@(mapcan #'(lambda (role)
+                  (make-role-links mop role))
+              (vis-roles mop data))))
+
+(defun make-abstraction-links (mop)
+  (mapcar #'(lambda (abstraction)
+              (net-vis:make-link :source mop :label "subClassOf" :target abstraction :data nil))
+          (mop-abstractions mop)))
+
+(defun make-role-links (mop role)
   (let ((fillers (inherit-filler mop role)))
-    (cond ((listp fillers) (mapcar #'net-vis:make-node fillers))
-          (t (list (net-vis:make-node fillers))))))
+    (if (listp fillers)
+        (mapcar #'(lambda (filler)
+                    (net-vis:make-link :source mop :label role :target filler :data (inherited-role? mop role)))
+                (inherit-filler mop role))
+        (list (net-vis:make-link :source mop :label role :target fillers :data (inherited-role? mop role))))))
+
+(defun make-path-link (source target)
+  (let ((relationship (find-relationship source target)))
+    ; Check if there is a relationship. Otherwise, assume the directionality is reversed
+    (if relationship
+        (net-vis:make-link :source source :label relationship :target target :data nil)
+        (net-vis:make-link :source target :label (find-relationship target source) :target source :data nil))))
+
+
+;;; NODES ;;;
+
+(defun make-mops-nodes (mops data)
+  (mapcan #'(lambda (mop) (make-mop-nodes mop data)) mops))
 
 (defun make-mop-nodes (mop data)
   `(,(net-vis:make-node mop)
     ,@(mapcar #'net-vis:make-node (mop-abstractions mop))
     ,@(mapcan #'(lambda (role) (make-filler-nodes mop role)) (vis-roles mop data))))
 
-(defun filter-roles (roles)
-  (remove-if #'(lambda (role) (equal :kabob-ids role)) roles))
+(defun make-filler-nodes (mop role)
+  (let ((fillers (inherit-filler mop role)))
+    (cond ((listp fillers) (mapcar #'net-vis:make-node fillers))
+          (t (list (net-vis:make-node fillers))))))
 
-(defun make-mops-nodes (mops data)
-  (mapcan #'(lambda (mop) (make-mop-nodes mop data)) mops))
 
-(defun make-mops-links (mops data)
-  (mapcan #'(lambda (mop) (make-mop-links mop data)) mops))
+;;; SEARCH ;;;
 
-(defun find-node (node-name)
-  (lookup-mop (intern node-name :KaBOB)))
-
-(defun run-search (ids search-type)
+(defun run-search (mops search-type)
   (cond ((equal search-type "intersection search")
-         (intersection-search (mapcar #'find-node ids) #'mop-neighbors :max-depth 2 :max-fanout 8))))
+        (intersection-search mops #'mop-neighbors :max-depth 2 :max-fanout 8)
+         )))
 
-(defun find-relationship (source target)
-  (let ((relationship nil))
-    (dolist (role (mop-roles source) 'done)
-     (dolist (filler (role-filler source role) 'done)
-       (when (equal filler target)
-         (setf relationship role))))
-    (unless relationship
-      (dolist (abstraction (mop-abstractions source) 'done)
-        (when (equal abstraction target)
-          (setf-relationship "subClassOf"))))
-    relationship))
+(setq mops (mapcar #'find-node (list "cellular tumor antigen p53 (human)"
+                                     "cellular tumor antigen p53")))
+(setq paths (run-search mops "intersection search"))
 
-(defun make-path-link (source target)
-  (make-link :source source
-             :label (KaBOB::find-relationship source target)
-             :target target
-             :data nil))
-
-
+;;;;;;;;;;;;;;;;;;;;;;; NET-VIS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Override net-vis methods
 (in-package :net-vis)
 
+
+;;; UTIL ;;;
 (defun make-data (inherited)
   (format nil "~a=~a" "inherited" (if inherited "true" "false")))
 
@@ -92,37 +105,32 @@
 	((listp x) (format nil "~{~a~^, ~}" (mapcar #'stringify x)))
 	(t (format nil "~a" x))))
 
+(defun process-paths (paths)
+  (do ((path (car paths) (car rest))
+       (rest paths (cdr rest))
+       (nodes nil (append (mapcar #'make-node path) nodes))
+       (links nil (append (process-path-links path) links)))
+
+      ((null rest) (make-json-graph nodes links))))
+
+(defun process-path-links (path)
+  (mapcar #'(lambda (previous-mop mop)
+              (KaBOB::make-path-link previous-mop mop))
+          path (cdr path)))
+
+;;; SEND ;;;
 (defun send-node-data (node-name &rest data)
-  (format t "node requested: ~a~%" node-name)
-  (format t "data requested: ~a~%" data)
+  (format t "node requested: ~a~%data requested: ~a~%" node-name data)
   (let ((mops (list (KaBOB::find-node node-name))))
     (make-json-graph
      (KaBOB::make-mops-nodes mops data)
      (KaBOB::make-mops-links mops data))))
 
 (defun send-search-results (ids search-type)
-  (process-paths (run-search ids search-type)))
+  (format t "search requested: ~a~%search type: ~a~%" ids search-type)
+  (process-paths (KaBOB::run-search (mapcar #'KaBOB::find-node ids) search-type)))
 
-(defun process-paths (paths)
-  (do ((path (car paths) (car rest))
-       (rest paths (cdr rest))
-       (nodes nil nodes)
-       (links nil links))
 
-      ((null rest) (make-json-graph nodes links))
-    (process-path path nodes links)))
-
-(defun process-path (path nodes links)
-  (do ((mop (car path) (car path-rest))
-       (path-rest path (cdr path))
-       (previous-mop nil mop))
-
-      ((null path-rest) 'done)
-    
-    (push (make-node mop) nodes)
-    ;; (when previous-mop
-      ;; (push (make-path-link previous-mop mop) links))
-    ))
-
+;;; AUTOCOMPLETE ;;;
 ;(setf *auto-complete-data* (make-autocomplete-tree-from-map KaBOB::*mops*))
 
